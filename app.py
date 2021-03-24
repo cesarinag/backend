d8e682437a2b0100940dca9722a27db36f8ba8b5
fahali <fahim.ali.204@gmail.com>
2021-03-24 16:56:01 -0400

    connected to data model and generating predictions

--- app.py
+++ app.py
@@ -1,14 +1,49 @@
 from flask import Flask
 from flask import request
 
-# import DSI library here
+import pickle
+import pandas
+import numpy
 
 app = Flask(__name__)
 
+# load our prediction model
+def load_model():
+    return pickle.load(open("./model/logr_gs.p", "rb"))
+
+
+# load our base data frame
+def load_df():
+    return pandas.read_csv("./model/X_test.csv")
+
+
+# enable specific features for our data frame
+def build_frame(params):
+    df = load_df()
+    df["nonprofit"] = params["nonprofit"]
+    df["jobsreported"] = params["jobsreported"]
+
+    zip = params["zip"]
+    df[f"zip_{zip}"] = 1
+
+    naics = params["naics"]
+    df[f"naicscode_{naics}"] = 1
+
+    lender = params["lender"]
+    df[f"lender_{lender}"] = 1
+
+    return df
+
+
+# generate our loan probabilities
+def predict_probabilites():
+    model = load_model()
+    df = build_frame(request.get_json())
+    return model.predict_proba(df)[0]
+
 
 @app.route("/", methods=["POST"])
-def gen_loans():
-    # get business data points using request.get_json()
-    # pass to DSI library
-    # return DSI library response as JSON
-    pass
+def gen_probabilites():
+    probabilities = predict_probabilites().tolist()
+    response = {"probabilities": probabilities}
+    return response
